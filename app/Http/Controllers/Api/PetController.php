6919ca0ecaa2a3c30ef2287ad07e73d1e4afd853
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-07-31 14:05:33 +0700

    pet index

--- app/Http/Controllers/Api/PetController.php
+++ app/Http/Controllers/Api/PetController.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\UseCase\Pet\IndexAction;
 use App\UseCase\Pet\StoreAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Pet\StoreRequest;
+use App\Http\Resources\PetResource;
+use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
+    /**
+     * @OA\Get(
+     *   tags={"Pet"},
+     *   path="/api/pets",
+     *   summary="get pet list",
+     *   @OA\Parameter(
+     *     name="user_id",
+     *     in="query",
+     *     description="user id",
+     *     required=false,
+     *     @OA\Schema(
+     *       type="integer",
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response="200",
+     *     description="OK",
+     *     @OA\JsonContent(
+     *       type="array",
+     *       @OA\Items(ref="#/components/schemas/PetResource")
+     *     )
+     *   )
+     * )
+     * 
+     */
+    public function index(Request $request, IndexAction $action)
+    {
+        return PetResource::collection($action($request->query('user_id')));
+    }
+
     /**
      * @OA\POST(
      *   tags={"Pet"},
